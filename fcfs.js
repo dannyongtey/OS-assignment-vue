b8193247e12848710a92b19b034973ca22150a21
Prev Wong <prevwong@gmail.com>
2018-09-25 12:57:23 +0800

    fixed fcfs

--- fcfs.js
+++ fcfs.js
@@ -1,8 +1,9 @@
 const fcfs = (input) => {
     const totalBurstTime = Object.keys(input).reduce((a, b) => a + input[b].burst, 0),
         sortedProcessId = Object.keys(input).sort((p1, p2) => (input[p1].arrival - input[p2].arrival));
-    let execution = {}, currentProcessId = sortedProcessId[0], t = 0;
+    let execution = {}, currentProcessId = sortedProcessId[0], t = 0, superiorOrder = [];
     while (t < totalBurstTime) {
+        // execution[t] = currentProcessId;
         const currentProcess = input[currentProcessId]; // get current process config
         const duration = t + currentProcess.burst;
         // get processes that arrive between t and duration where priority is higher than the current process
@@ -12,19 +13,22 @@ const fcfs = (input) => {
             // if no superior process arrive between this timeframe, we will finish the current process first
             currentProcess.completed = true;
             t = duration;
+            execution[t] = currentProcessId;
             // then find the next best process in the waiting list
             const waitingList = sortedProcessId.filter((p) => !input[p].completed);
-            const superior = waitingList.sort((p1, p2) => input[p1].priority - input[p2].priority);
-            if ( superior[0] ) currentProcessId = superior[0];
+            superiorOrder = waitingList.sort((p1, p2) => input[p1].priority - input[p2].priority);
         } else {
             // else get the process that arrived first 
-            const superior = arrivedProcesses[0];
-            const diff = input[superior].arrival - t;
+            superiorOrder = arrivedProcesses;
+            const diff = input[superiorOrder[0]].arrival - t;
             input[currentProcessId].burst -= diff; // update the current process burst
             t += diff;
-            currentProcessId = superior;
         }
+
         execution[t] = currentProcessId;
+        currentProcessId = superiorOrder[0];
+        
+       
     }
     return execution
 }
