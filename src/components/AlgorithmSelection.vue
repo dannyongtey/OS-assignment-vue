cc652e86bf7f1eef24841569752eca4a7b8089e2
dannyongtey <dannyongtey@gmail.com>
2018-09-25 10:38:37 +0800

    Shift folder to root

---
+++ src/components/AlgorithmSelection.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		{{ algorithm }}
+
+		<select >
+
+		</select>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'AlgorithmSelection',
+		props: ['algorithm']
+	}
+</script>
+
+<style scoped>
+	
+</style>
